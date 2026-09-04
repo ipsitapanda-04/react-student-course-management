0718aaf2882ee3174373dff0c33dc717d8be7067
ipsitapanda-04 <ipsitapanda081@gmail.com>
2026-09-04 18:09:55 +0530

    Create home.jsx

---
+++ home.jsx
@@ -0,0 +1,19 @@
+import { useContext } from "react";
+import { StudentContext } from "../context/StudentContext";
+
+function Home() {
+  const student = useContext(StudentContext);
+
+  return (
+    <div>
+      <h1>Student Course Management</h1>
+
+      <h2>Student Information</h2>
+      <p>Name: {student.name}</p>
+      <p>Course: {student.course}</p>
+      <p>College: {student.college}</p>
+    </div>
+  );
+}
+
+export default Home;
