9369e640e39854537355cc11c69701ce49093f46
ipsitapanda-04 <ipsitapanda081@gmail.com>
2026-09-04 18:11:36 +0530

    Create about.jsx

---
+++ about.jsx
@@ -0,0 +1,12 @@
+function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        This application helps students manage and view their courses.
+      </p>
+    </div>
+  );
+}
+
+export default About;
